3471a0964f5c60b66fe6c10865b7c790d14b4be7
EPC233 <echou@unca.edu>
2025-02-04 16:37:58 -0500

    added hash function to .py

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -7,6 +7,17 @@ def find_duplicates_nested_loop(l: list) -> list:
     return duplicates
 
 
+def find_duplicates_hash_set(l: list) -> list:
+    seen = set()
+    duplicates = set()
+    for item in l:
+        if item in seen:
+            duplicates.add(item)
+        else:
+            seen.add(item)
+    return list(duplicates)
+
+
 if __name__ == "__main__":
     sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
     sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
@@ -17,3 +28,10 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+
+    print("Hash Set Method")
+
+    print("Sample 1:", find_duplicates_hash_set(sample1))
+    print("Sample 2:", find_duplicates_hash_set(sample2))
+    print("Sample 3:", find_duplicates_hash_set(sample3))
+    print("Sample 4:", find_duplicates_hash_set(sample4))
